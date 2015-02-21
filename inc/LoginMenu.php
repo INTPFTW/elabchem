bbea4b6d4a4fa1996fab3534c29490133207dd46
j0h <mf0hka@gmail.com>
2015-02-20 20:02:42 -0500

    Update and rename inc to inc/LoginMenu.php
    Removed links and references to footer that held data inaccessible to users not logged in.

---
+++ inc/LoginMenu.php
@@ -0,0 +1,100 @@
+<?php
+/********************************************************************************
+*  This file is part of eLabChem (http://github.com/martinp23/elabchem)         *
+*  Copyright (c) 2013 Martin Peeks (martinp23@googlemail.com)                   *
+*                                                                               *
+*    eLabChem is free software: you can redistribute it and/or modify           *
+*    it under the terms of the GNU Affero General Public License as             *
+*    published by the Free Software Foundation, either version 3 of             *
+*    the License, or (at your option) any later version.                        *
+*                                                                               *
+*    eLabChem is distributed in the hope that it will be useful,                *
+*    but WITHOUT ANY WARRANTY; without even the implied                         *
+*    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR                    *
+*    PURPOSE.  See the GNU Affero General Public License for more details.      *
+*                                                                               *
+*    You should have received a copy of the GNU Affero General Public           *
+*    License along with eLabChem.  If not, see <http://www.gnu.org/licenses/>.  *
+*                                                                               *
+*   eLabChem is a fork of elabFTW. This file incorporates work covered by the   *
+*   copyright notice below.                                                     *                                                               
+*                                                                               *
+********************************************************************************/
+
+/********************************************************************************
+*                                                                               *
+*   Copyright 2012 Nicolas CARPi (nicolas.carpi@gmail.com)                      *
+*   http://www.elabftw.net/                                                     *
+*                                                                               *
+********************************************************************************/
+
+/********************************************************************************
+*  This file is part of eLabFTW.                                                *
+*                                                                               *
+*    eLabFTW is free software: you can redistribute it and/or modify            *
+*    it under the terms of the GNU Affero General Public License as             *
+*    published by the Free Software Foundation, either version 3 of             *
+*    the License, or (at your option) any later version.                        *
+*                                                                               *
+*    eLabFTW is distributed in the hope that it will be useful,                 *
+*    but WITHOUT ANY WARRANTY; without even the implied                         *
+*    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR                    *
+*    PURPOSE.  See the GNU Affero General Public License for more details.      *
+*                                                                               *
+*    You should have received a copy of the GNU Affero General Public           *
+*    License along with eLabFTW.  If not, see <http://www.gnu.org/licenses/>.   *
+*                                                                               *
+********************************************************************************/
+require_once('admin/config.php');
+?>
+<noscript><!-- show warning if javascript is disabled -->
+<div class="ui-state-error ui-corner-all">
+<p><span class='ui-icon ui-icon-alert' style='float:left; margin: 0 5px 0 5px;'></span>
+<strong>Javascript is disabled.</strong> Please enable Javascript to view this site in all its glory. Thank You.</p>
+</div>
+</noscript>
+
+<div id='logo'>
+<img src='img/logo-169.png' alt='elabchem' />
+</div>
+
+<div id="logmenu"><p>
+<?php
+if (isset($_SESSION['auth']) && $_SESSION['is_admin'] === '1') {
+?>
+    <!-- ADMIN MENU --> 
+    <!-- <a href='https://twitter.com/elabftw'>
+        <img src='img/twitter-bird-16x16.png' alt='twitter' title='Follow eLabFTW on Twitter !'>
+    </a> |  -->
+    <a id='check_for_updates' href='#'>Check for updates</a> | <a href='admin.php'>Admin Panel</a> | 
+    <script>
+    $('#check_for_updates').click(function() {
+        var jqxhr = $.post('check_for_updates.php', function(answer) {
+            alert(answer);
+        });
+    });
+    </script>
+    <?php
+    }
+if (isset($_SESSION['auth']) && $_SESSION['auth'] === 1) {
+    echo "Logged in as <a href='profile.php' title='Profile'>".$_SESSION['username']."</a> | 
+        <a href='ucp.php'><img src='themes/".$_SESSION['prefs']['theme']."/img/pref.png' alt='Control panel' title='Control panel' /></a> | 
+        <a href='logout.php'><img src='themes/".$_SESSION['prefs']['theme']."/img/logout.png' alt='' title='Logout' /></a></p>";
+} else {
+    echo "<a href='login.php'>Not logged in !</a>";
+}
+?>
+</div>
+<hr class='flourishes'>
+<!-- TITLE -->
+<div id='page_title'>
+<h2><?php echo strtoupper($page_title);?></h2>
+</div>
+<?php
+if (DEBUG) {
+    echo "Session array : ";
+    print_r($_SESSION);
+    echo "<br />";
+}
+?>
+
